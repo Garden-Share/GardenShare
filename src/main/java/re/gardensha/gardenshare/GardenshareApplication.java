456ef2f45edb6ff59e4852a16abc5cc74b396f40
Alex Gravenor <blazingkin@gmail.com>
2018-10-08 13:22:55 -0700

    Properly set up eclipse project.

---
+++ src/main/java/re/gardensha/gardenshare/GardenshareApplication.java
@@ -0,0 +1,12 @@
+package re.gardensha.gardenshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenshareApplication.class, args);
+	}
+}
