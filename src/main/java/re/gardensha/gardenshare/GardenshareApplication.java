c4332b98a36134f12b98841d0ab50262d9ecf3a9
Michael Curtis <michaelcurtis96@gmail.com>
2018-10-09 01:12:43 -0700

    + Google Auth. If auth is successful, redirect to /user which dumps the Principal obj for that user

--- src/main/java/re/gardensha/gardenshare/GardenshareApplication.java
+++ src/main/java/re/gardensha/gardenshare/GardenshareApplication.java
@@ -1,12 +1,22 @@
 package re.gardensha.gardenshare;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GardenshareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GardenshareApplication.class, args);
 	}
+	
+	@RequestMapping(value = "/user")
+   public Principal user(Principal principal) {
+      return principal;
+   }
 }
