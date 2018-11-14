cea5eef8ade1eecdfc1d20d2ffa2576d9b7dd007
Alex Gravenor <blazingkin@gmail.com>
2018-11-14 12:08:09 -0800

    Fix problem by switching to Controller instead of RestController

--- src/main/java/re/gardensha/gardenshare/IndexController.java
+++ src/main/java/re/gardensha/gardenshare/IndexController.java
@@ -1,17 +1,10 @@
 package re.gardensha.gardenshare;
 
-import java.io.IOException;
-import java.util.Optional;
-
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
