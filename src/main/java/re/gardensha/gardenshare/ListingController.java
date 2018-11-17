d2eb7e069f30b000b293215e11702f4742cc6c20
Alex Gravenor <blazingkin@gmail.com>
2018-11-17 13:28:47 -0800

    Update parser, add form that has all the required fields

--- src/main/java/re/gardensha/gardenshare/ListingController.java
+++ src/main/java/re/gardensha/gardenshare/ListingController.java
@@ -3,8 +3,10 @@ package re.gardensha.gardenshare;
 import java.io.IOException;
 import java.sql.Time;
 import java.text.DateFormat;
-import java.util.Optional;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,6 +24,11 @@ public class ListingController {
     private ListingRepository listingRepository;
 
     private static String listingObjectName = "listing";
+    // Use ISO 8601 date format
+    private static DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+    private static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    private static Logger logger = Logger.getGlobal();
 
 	@GetMapping(path="/listing")
 	public ModelAndView getListing(@RequestParam(value="id") int id, HttpServletResponse res) throws IOException {
@@ -47,14 +54,14 @@ public class ListingController {
         Time start, end;
         try {
             if (startTimeStamp.isPresent()){
-                long startMillis = DateFormat.getInstance().parse(startTimeStamp.get()).getTime();
-                start = new Time(startMillis);
+                start = parseTime(startTimeStamp.get());
             }else{
                 start = new Time(System.currentTimeMillis());
             }
-            long endMillis = DateFormat.getInstance().parse(endTimeStamp).getTime();
-            end = new Time(endMillis);
+            end = parseTime(endTimeStamp);
         }catch(ParseException exception){
+            logger.info("Start param was "+startTimeStamp);
+            logger.info("End param was "+endTimeStamp);
             throw new InvalidListingException("Start or end date had an invalid date format");
         }
         Listing newListing = new Listing(type, weight, weightUnit, count.orElse(-1), start, end);
@@ -63,5 +70,15 @@ public class ListingController {
         return result;
     }
 
+    private Time parseTime(String timeStamp) throws ParseException {
+        try {
+            // Try parsing with ISO format first
+            return new Time(format.parse(timeStamp).getTime());
+        }catch(ParseException e){
+
+        }
+        // If it doesn't work, parse with just the date format
+        return new Time(dateFormat.parse(timeStamp).getTime());
+    }
 	
 }
