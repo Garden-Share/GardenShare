762dc198fe503e7d8cd02a35bb3fdd7b0d89d87a
Alex Gravenor <blazingkin@gmail.com>
2018-11-23 17:56:23 -0800

    Add test for listing controller

---
+++ src/test/java/re/gardensha/gardenshare/ListingControllerTest.java
@@ -0,0 +1,41 @@
+package re.gardensha.gardenshare;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ListingControllerTest {
+	
+	@LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ListingRepository listingRepository;
+
+    @Test
+    public void postingAListingWithNoParametersShouldNotAddListing() throws Exception {
+        long listingCount = listingRepository.count();
+        this.restTemplate.postForObject("http://localhost:" + port + "/lisiting/new", null, String.class);
+        assertThat(listingCount).isEqualTo(listingRepository.count());
+    }
+
+	@Autowired
+	private ListingController controller;
+	
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+	
+}
