4256eae6b0a4250a615ec39dc056d2fb6a777cea
Jaedo Han <jhan0801@gmail.com>
2018-11-25 10:49:35 -0800

    removed Jackson DataBinding for now

--- src/main/java/re/gardensha/gardenshare/WebSocketConfig.java
+++ src/main/java/re/gardensha/gardenshare/WebSocketConfig.java
@@ -1,23 +1,23 @@
-package re.gardensha.gardenshare;
+// package re.gardensha.gardenshare;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+// @Configuration
+// @EnableWebSocketMessageBroker
+// public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
  
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/chatapplication");
-        config.setApplicationDestinationPrefixes("/message");
-    }
+//     @Override
+//     public void configureMessageBroker(MessageBrokerRegistry config) {
+//         config.enableSimpleBroker("/chatapplication");
+//         config.setApplicationDestinationPrefixes("/message");
+//     }
  
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-         registry.addEndpoint("/chat").withSockJS();
-    }
-}
+//     @Override
+//     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//          registry.addEndpoint("/chat").withSockJS();
+//     }
+// }
