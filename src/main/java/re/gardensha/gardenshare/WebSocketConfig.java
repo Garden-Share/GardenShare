a1016078ec17a71db9e6f905f7e5018cfd8dc4fa
Jaedo Han <jhan0801@gmail.com>
2018-11-25 10:39:21 -0800

    added websocket config for chat

--- src/main/java/re/gardensha/gardenshare/WebSocketConfig.java
+++ src/main/java/re/gardensha/gardenshare/WebSocketConfig.java
@@ -12,13 +12,12 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
  
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/chatapplication");
+        config.setApplicationDestinationPrefixes("/message");
     }
  
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-         registry.addEndpoint("/chat");
          registry.addEndpoint("/chat").withSockJS();
     }
 }
