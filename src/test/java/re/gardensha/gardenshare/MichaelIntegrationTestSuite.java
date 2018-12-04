4cb7f7a8d6ceea32f109add9f31a631a480f5d3b
michaelcurtis <michaelcurtis@10.144.243.14>
2018-12-03 16:13:12 -0800

    added chat unit test

---
+++ src/test/java/re/gardensha/gardenshare/MichaelIntegrationTestSuite.java
@@ -0,0 +1,18 @@
+package re.gardensha.gardenshare;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    GardenshareApplicationTests.class,
+    HealthTest.class,
+    IndexTest.class,
+    ListingControllerTest.class,
+    ReviewControllerTest.class
+})
+
+public class MichaelIntegrationTestSuite {
+
+}
---
+++ src/test/java/re/gardensha/gardenshare/MichaelUnitTestSuite.java
@@ -0,0 +1,14 @@
+package re.gardensha.gardenshare;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    ChatTest.class
+})
+
+public class MichaelUnitTestSuite {
+
+}
