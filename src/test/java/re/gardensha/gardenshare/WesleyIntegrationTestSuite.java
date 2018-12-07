22f8021a343dd2ee437e30b4b960ef595a4868a9
wbenica <wtbenica@gmail.com>
2018-12-06 18:31:38 -0800

    Revert "Integration Test - User Controller"
    This reverts commit b5e2ccfa8b25b39d87762c89c39beeaa2cb9f94e.

--- src/test/java/re/gardensha/gardenshare/WesleyIntegrationTestSuite.java
+++ src/test/java/re/gardensha/gardenshare/WesleyIntegrationTestSuite.java
@@ -1,14 +1,5 @@
 package re.gardensha.gardenshare;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-
-@Suite.SuiteClasses({
-	UserControllerTest.class
-})
-
 public class WesleyIntegrationTestSuite {
-    // These tests are by Wesley Benica
+
 }
